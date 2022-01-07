bc3b647c7f78ea4f4cca1d9d200c27b3f9b14693
arsyabhrn <alfatehanarsya8998@gmail.com>
2022-01-07 20:07:46 +0700

    java substring

--- src/codinginterview/JavaDequeue/test.java
+++ src/codinginterview/JavaDequeue/test.java
@@ -5,15 +5,87 @@ public class test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Deque deque = new ArrayDeque<>();
+        ArrayList arrays = new ArrayList<Integer>();
+        Set<Integer> set = new Set<Integer>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Integer> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Integer integer) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Integer> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+        };
         int n = in.nextInt();
         int m = in.nextInt();
+
+        System.out.println(n);
+        System.out.println(m);
 
         for (int i = 0; i < n; i++) {
             int num = in.nextInt();
             deque.offer(num);
-            if (deque.size() == 3){
-                deque.stream().sorted();
-                deque.poll();
+            set.add(num);
+            if (deque.size() == m){
+                arrays.add(set.size());
+                set.remove(deque.poll());
+                System.out.println(arrays.size());
             }
         }
     }
